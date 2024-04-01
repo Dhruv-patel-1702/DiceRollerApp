bc43663e365c0f6b8da4d124c5c88267eac2b17b
Dhruv-patel-1702 <111880388+Dhruv-patel-1702@users.noreply.github.com>
2024-04-01 18:42:35 +0530

    dice roller page complete

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+            Color.fromARGB(255, 33, 5, 109),
+            Color.fromARGB(255, 68, 21, 149)),
+      ),
+    ),
+  );
+}
